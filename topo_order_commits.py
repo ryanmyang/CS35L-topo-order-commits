88e9a208ecc977e3e10aeb656a219b2f34fed12c
RyanMYang <32602114+ryanmyang@users.noreply.github.com>
2023-06-02 13:47:34 -0700

    topological ordering + formatted printing

--- topo_order_commits.py
+++ topo_order_commits.py
@@ -95,21 +95,69 @@ def build_commit_graph(git_directory):
 def get_local_branches(git_directory):
     refs_directory = os.path.join(git_directory, '.git', 'refs', 'heads')
 
-    branch_names = []
+    branches = {}
     for root, _, files in os.walk(refs_directory):
         for file in files:
-            if not all(c in '0123456789abcdef' for c in file):
-                continue
             branch_path = os.path.join(root, file)
             with open(branch_path, 'r') as f:
-                branch_name = f.read().strip()
-                branch_names.append(branch_name)
+                branch_id = f.read().strip()
+                branches.setdefault(branch_id, []).append(file)
+    return branches
 
-    return branch_names
+def get_topological_order(commit_graph, root_commits):
+    visited = set()
+    order = []
 
-def explain_directories():
-    print("The 'refs' directory contains references to different Git objects, such as branches and tags.")
-    print("The 'objects' directory stores the actual Git objects, such as blobs, trees, and commits.")
+    def dfs(commit_hash):
+        visited.add(commit_hash)
+
+        # Traverse children first
+        for child_hash in commit_graph[commit_hash].children:
+            if child_hash not in visited:
+                dfs(child_hash)
+
+        # Append the current commit to the order
+        order.append(commit_hash)
+
+    # Perform depth-first search starting from each root commit
+    for root_commit in root_commits:
+        if root_commit not in visited:
+            dfs(root_commit)
+
+
+    return order
+
+
+def print_commit_order(commit_order, commit_graph, branch_names):
+    prev = None
+
+    for commit_hash in commit_order:
+        commit_node = commit_graph[commit_hash]
+
+        # Check if a sticky end needs to be inserted
+        # "If the next commit to be printed is not the parent of the current commit, insert a "sticky end""
+        # The “sticky end” will contain the commit hashes of the parents of the current commit
+        if prev and commit_hash not in commit_graph[prev].parents:
+            print(" ".join(commit_graph[prev].parents) + " =", end="\n\n")
+            sticky_start = "= " + " ".join(commit_node.children)
+            print(sticky_start)
+            
+
+        # Print the commit hash
+        print(commit_hash, end=" ")
+
+        # Print branch names if the commit corresponds to a branch head
+        if commit_hash in branch_names:
+            branches = sorted(branch_names[commit_hash])
+            print(" ".join(branches), end="")
+
+        print()
+
+        # Update sticky end and reset sticky start
+        prev = commit_hash
+
+
+
 
 if __name__ == "__main__":
     # CHANGE OF DIRECTORY FOR TESTING PURPOSES
@@ -119,8 +167,10 @@ if __name__ == "__main__":
     git_directory = find_git_directory()
     branch_names = get_local_branches(git_directory)
     graph, roots = build_commit_graph(git_directory)
-    print("Local Branches:", branch_names)
-    print("Commit Graph:", graph)
-    print("Roots:", roots)
+    # print("Local Branches:", branch_names)
+    # print("Commit Graph:", graph)
+    # print("Roots:", roots)
 
-    explain_directories()
+    topological_order = get_topological_order(graph, roots)
+    # print("Topological Order:", topological_order)
+    print_commit_order(topological_order, graph, branch_names)
